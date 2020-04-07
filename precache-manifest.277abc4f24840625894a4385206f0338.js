96e19827fcd2b57d522b8b17f5f0a7c8e252a766
willissa2121 <you@example.com>
2020-04-07 13:13:58 -0700

    Updates

---
+++ precache-manifest.277abc4f24840625894a4385206f0338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a85ef107bbf92b12ad12c280c58571e",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "4518208cc41cf25cc361",
+    "url": "/dnd/static/css/main.4101dd58.chunk.css"
+  },
+  {
+    "revision": "361f7b5b221cdf282214",
+    "url": "/dnd/static/js/2.e5fa35fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd/static/js/2.e5fa35fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4518208cc41cf25cc361",
+    "url": "/dnd/static/js/main.230746cd.chunk.js"
+  },
+  {
+    "revision": "fa71e0d61fe7f8cef7bc",
+    "url": "/dnd/static/js/runtime-main.b46cc71e.js"
+  }
+]);

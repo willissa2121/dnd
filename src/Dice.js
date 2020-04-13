bdc2ace628e2e4621810c368556da69b30783e95
willissa2121 <you@example.com>
2020-04-12 19:15:05 -0700

    sounds randomely started working

--- src/Dice.js
+++ src/Dice.js
@@ -4,12 +4,17 @@ const diceVal = [4, 6, 8, 10, 20];
 const colorVal = ["danger", "warning", "success", "primary", "info"];
 
 export default function Dice(props) {
+
+  const audio = new Audio(require('./public/sounds/diceSound.mp3'))
   return (
     <div>
       {diceVal.map((val, iteration) => (
         <button
           className={`btn btn-${colorVal[iteration]} mr-1 ml-1`}
           onClick={() => {
+            audio.play().then(val=>{
+              console.log('rolled')
+            })
             props.random(val, props.user);
           }}
           value={val}

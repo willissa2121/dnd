21ae4bebdcf6ff27d3cd302b1fb64fdf9bf01f48
willissa2121 <you@example.com>
2020-04-17 13:50:39 -0700

    using has router cause github pages is ass

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./mainPage/Users.js";
 import "./App.css";
 import Main from "./mainPage/Main";
 import Admin from "./adminPage/Admin";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
-      <div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/admin" component={Admin} />
-      </div>
+        <Route path="/admin" component={Admin} />
+      </Switch>
     </Router>
   );
 }
